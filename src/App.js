41fa2916dda278d036ab1d867b25ec87f82fafd5
Dipo Arowona <abdulsalam.arowona@gmail.com>
2020-07-24 01:00:28 -0400

    initial Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./CSS/App.css";
 
 function App() {
+  const [color, setColor] = useState("White");
+  const [transparency, setTransparency] = useState(100);
+
+  // const listenScrollEvent = (e) => {
+  //   if (window.scrollY > 100) {
+  //     setColor("black");
+  //   } else {
+  //     setColor("white");
+  //   }
+  // };
+
+  // useEffect(() => {
+  //   window.addEventListener("scroll", listenScrollEvent);
+  // });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div /*style={{ background: color }}*/>
+      <div id="landingpage">
+        <h1 landing-text>Meek Mill</h1>
+        <video autoPlay loop muted>
+          <source
+            src="https://api.meekmill.com/wp-content/uploads/2018/11/mm-tidal-vid.mp4"
+            type="video/mp4"
+          />
+        </video>
+      </div>
+      <div className="content">
+        <h1>brand new section</h1>
+      </div>
     </div>
   );
 }
